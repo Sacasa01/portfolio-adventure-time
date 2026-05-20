c39707e73dd292cde97caf74e495c5f2202266aa
Santiago Castro Salt <sacasa01@alumnatflorida.es>
2026-05-20 16:13:12 +0200

    🔥 clean: reset router configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,3 @@
 import { Routes } from '@angular/router';
-import { TreehouseHome } from './components/treehouse-home/treehouse-home';
-import { PrismoAbout } from './components/prismo-about/prismo-about';
-import { CandyProjects } from './components/candy-projects/candy-projects';
-import { MarcelineSkills } from './components/marceline-skills/marceline-skills';
-import { IceKingContact } from './components/ice-king-contact/ice-king-contact';
 
-export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: TreehouseHome },
-  { path: 'about', component: PrismoAbout },
-  { path: 'projects', component: CandyProjects },
-  { path: 'skills', component: MarcelineSkills },
-  { path: 'contact', component: IceKingContact },
-  { path: '**', redirectTo: 'home' }
-];
-
+export const routes: Routes = [];
